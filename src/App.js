dabc12e2f9bf22b74242f0f3db556ca5749c14fb
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-04-01 12:27:35 +0530

    Understood React Component Props.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-const Person = () => {
+const Person = (props) => {
   return (
     <>
-    <h1>Name: Khizr</h1>
-    <h2>Last Name: Shaikh</h2>
-    <h2>Age: 17</h2>
+    <h1>Name: {props.name}</h1>
+    <h2>Last Name: {props.lastName}</h2>
+    <h2>Age: {props.age}</h2>
     </>
   )
 }
@@ -13,11 +13,12 @@ const Person = () => {
 const App = () => {
   return (
     <div className="App">
-      <Person />
-      <Person />
-      <Person />
-      <Person />
-      <Person />
+      <Person
+        name='Khizr' 
+        lastName='Shaikh' 
+        age={17} 
+      />
+      <Person name='Saad' lastName='Khan' age={21} />
     </div>
   );
 }
