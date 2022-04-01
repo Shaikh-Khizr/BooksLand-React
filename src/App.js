496ca246e02113343b91846e31515edfc0d216ce
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-04-01 14:44:18 +0530

    Understood useState HOOK and Events.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
-
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <Person
-        name='Khizr' 
-        lastName='Shaikh' 
-        age={17} 
-      />
-      <Person name='Saad' lastName='Khan' age={21} />
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
